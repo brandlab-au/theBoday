d39224c4cf3187b3707bfe977bd6391041c74be0
Jon Mckenzies <jon@brandlab.co.uk>
2017-09-26 12:51:11 -0500

    home page

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -9,19 +9,30 @@ import { ItemDetailsPage } from '../item-details/item-details';
   templateUrl: 'list.html'
 })
 export class ListPage {
+  sport: string[];
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  color: string[];
+  items: Array<{title: string, note: string, icon: string,color: string}>;
+   
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
+      
+      this.sport = ['Bodybuilding', 'Strength', 'Running', 'Football', 'Basketball', 'Ridding',
+    'American-football', 'Triathlons', 'Swimming', 'CrossFit','Boxing'];
+      
+      
+      this.color = ['background-color: red', 'background-color: blue', 'background-color: green', 'background-color: black', 'background-color: orange', 'background-color: red',
+    'background-color: gray', 'background-color: yellow', 'background-color: green', 'background-color: pink','background-color: brown'];
 
     this.items = [];
     for(let i = 1; i < 11; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        title: this.sport[i]+' '+ i,
+        note: 'See more' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)],
+        color: this.color[i]
       });
     }
   }
